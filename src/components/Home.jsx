8075cca059148579eff2d3eb119f6b35a8d7befe
EkfrazoMohammed <mohammed@ekfrazo.in>
2024-10-17 10:57:12 +0530

    scrolling effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import Banner from './HomeComponents/Banner'
-import MidSection from './HomeComponents/MidSection'
-import CourseBanner from './HomeComponents/CourseBanner'
-import Descriptions from './HomeComponents/Descriptions'
-import IndustryApplications from './HomeComponents/IndustryApplications'
-import Locations from './HomeComponents/Locations'
-import FAQ from './HomeComponents/FAQ'
-import Partners from './HomeComponents/Partners'
-import Contacts from './HomeComponents/Contacts'
+import React from 'react';
+import Banner from './HomeComponents/Banner';
+import MidSection from './HomeComponents/MidSection';
+import CourseBanner from './HomeComponents/CourseBanner';
+import Descriptions from './HomeComponents/Descriptions';
+import IndustryApplications from './HomeComponents/IndustryApplications';
+import Locations from './HomeComponents/Locations';
+import FAQ from './HomeComponents/FAQ';
+import Partners from './HomeComponents/Partners';
+import Contacts from './HomeComponents/Contacts';
 
 const Home = () => {
   return (
     <div>
-      <Banner />
-      <MidSection />
-      <CourseBanner />
-      <Descriptions />
-    <IndustryApplications />
+      <div id="banner">
+        <Banner />
+      </div>
+      <div id="mid-section">
+        <MidSection />
+      </div>
+      <div id="course-banner">
+        <CourseBanner />
+      </div>
+      <div id="descriptions">
+        <Descriptions />
+      </div>
+      <div id="industry-applications">
+        <IndustryApplications />
+      </div>
+      <div id="faq">
         <FAQ />
-      <Partners />
-      <Locations />
-      <Contacts />
+      </div>
+      <div id="partners">
+        <Partners />
+      </div>
+      <div id="locations">
+        <Locations />
+      </div>
+      <div id="contacts">
+        <Contacts />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,37 @@
-import { Link } from 'react-router-dom'
+import { useNavigate,Link } from 'react-router-dom';
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path, id) => {
+    navigate(path);
+    setTimeout(() => {
+      const section = document.getElementById(id);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 100); // Delay to allow the page to render before scrolling
+  };
+
   return (
-    <div className='bg-white text-black flex md:flex-row md:px-32 md:py-6 gap-3 md:gap-8'>
-      <div className="image-container w-[80px] h-80px]">
+    <div className="bg-white text-black flex md:flex-row md:px-32 md:py-6 gap-3 md:gap-8">
+      <div className="image-container w-[80px] h-[80px]">
         <Link to="/">
-        {/* https://aactxg.stripocdn.email/content/guids/CABINET_f37167ea2322984dfeb6a0a05e92d2480b49356b15fb055bb2ce2e84131a12e4/images/vector_01.JPG */}
-        <img className="h-auto w-auto object-cover" src="https://aactxg.stripocdn.email/content/guids/CABINET_f37167ea2322984dfeb6a0a05e92d2480b49356b15fb055bb2ce2e84131a12e4/images/flydro_logo_png_1.png" alt="" />
-      </Link>
+          <img
+            className="h-auto w-auto object-cover"
+            src="https://aactxg.stripocdn.email/content/guids/CABINET_f37167ea2322984dfeb6a0a05e92d2480b49356b15fb055bb2ce2e84131a12e4/images/flydro_logo_png_1.png"
+            alt=""
+          />
+        </Link>
       </div>
       <div className="text-container flex justify-center items-center gap-2 md:gap-8 text-[1.1rem] font-medium">
-        <Link to="/course">Course</Link>
-        <Link to="/about">About us</Link>
+        <button onClick={() => handleNavigation('/', 'course-banner')}>Course</button>
+        <button onClick={() => handleNavigation('/', 'descriptions')}>About Us</button>
         <Link to="/careers">Careers</Link>
-        <Link to="/benefits">Benefits</Link>
-
+        <button onClick={() => handleNavigation('/', 'industry-applications')}>Benefits</button>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
